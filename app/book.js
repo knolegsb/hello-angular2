12dc0903714b8972756595851d7891a9307df65b
Sean <seon2you@hotmail.com>
2017-05-14 23:22:29 -0700

    added type

---
+++ app/book.js
@@ -0,0 +1,14 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var Book = (function () {
+    function Book(id, name, price, date, img) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.date = date;
+        this.img = img;
+    }
+    return Book;
+}());
+exports.Book = Book;
+//# sourceMappingURL=book.js.map
--- app/book.service.js
+++ app/book.service.js
@@ -7,17 +7,25 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var core_1 = require("@angular/core");
+var book_1 = require("./book");
 var BookService = (function () {
     function BookService() {
     }
     BookService.prototype.getBooks = function () {
-        return [
-            { id: '001', name: '타이젠 모바일 프로그래밍', price: 33000, date: '20150115', img: 'img001.jpg' },
-            { id: '002', name: '아파치 코르도바', price: 22000, date: '20150131', img: 'img002.jpg' },
-            { id: '003', name: '웹앱 하이브리드 앱', price: 32000, date: '20150320', img: 'img003.jpg' },
-            { id: '004', name: 'OpenGL ES 2.0 안드로이드 게임프로그래밍', price: 34000, date: '20150601', img: 'img004.jpg' },
-            { id: '005', name: 'Ext JS6 프로그래밍', price: 32000, date: '20151005', img: 'img005.jpg' },
-        ];
+        // return  [
+        //     {id:'001', name:'타이젠 모바일 프로그래밍', price:33000, date:'20150115', img:'img001.jpg'},
+        //     {id:'002', name:'아파치 코르도바', price:22000, date:'20150131', img:'img002.jpg'},
+        //     {id:'003', name:'웹앱 하이브리드 앱', price:32000, date:'20150320', img:'img003.jpg'},
+        //     {id:'004', name:'OpenGL ES 2.0 안드로이드 게임프로그래밍', price:34000, date:'20150601', img:'img004.jpg'},
+        //     {id:'005', name:'Ext JS6 프로그래밍', price:32000, date:'20151005', img:'img005.jpg'},
+        // ]   
+        this.books = [];
+        this.books.push(new book_1.Book('001', '타이젠 모바일 프로그래밍', 33000, '20150115', 'img001.jpg'));
+        this.books.push(new book_1.Book('002', '아파치 코르도바', 22000, '20150131', 'img002.jpg'));
+        this.books.push(new book_1.Book('003', '웹앱 하이브리드 앱', 29000, '20150320', 'img003.jpg'));
+        this.books.push(new book_1.Book('004', 'OpenGL ES 2.0 안드로이드 게임프로그래밍', 35000, '20150601', 'img004.jpg'));
+        this.books.push(new book_1.Book('005', 'Ext JS6 프로그래밍', 32000, '20150115', 'img005.jpg'));
+        return this.books;
     };
     return BookService;
 }());
